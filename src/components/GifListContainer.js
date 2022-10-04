6a8c48901778a74dde3a6cae26d0c6dbda89326f
123ZOKzok <user email>
2022-10-05 01:42:04 +0300

    done

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -3,18 +3,19 @@ import {useState, useEffect} from "react";
 import GifList from "./GifList";
 import GifSearch from "./GifSearch";
 
-const API_URL = 'https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g';
+const API_URL = 'https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=3pDtkgGBkaepFV6smB9O6rBAlQ83VLiO&rating=g';
 
 function GifListContainer  () {
     const [searchTerm, setSearchTerm] = useState("");
     const [gifs, setGifs] = useState([]);
     useEffect(() => {
         fetch(`${API_URL}`)
-        .then(res => res.json())
-        .then(data => {
-        console.log(data.data)
-        setGifs([...data.data]);
-        })
+        .then((res) => res.json())
+        .then(({ data }) => {
+          const gifs = data.map((gif) => ({ url: gif.images.original.url }));
+          setGifs(gifs);
+        });
+        
     }, []);
   
     return (
